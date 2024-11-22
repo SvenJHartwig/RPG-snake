19aa013aea9a086494b19f9cbcb498b013637154
sven <sven.hartwig@posteo.de>
2024-11-22 13:42:42 +0100

    Add engineTests

---
+++ engineTest.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch.hpp>
+#include "view/engine/elements/button.h"
+
+TEST_CASE("Button produces a colored rectangle")
+{
+    Button *button = new Button(0, 0, 20, 20);
+    button->text = "Test";
+    RenderData *data = button->createRenderData();
+    REQUIRE(button->text == "Test");
+    REQUIRE(data->getVertices().size() == 4);
+    REQUIRE(data->getIndices().size() == 6);
+}

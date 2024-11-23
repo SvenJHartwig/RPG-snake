14352c26fdf7e93f096a23f3d97b569351c01e7f
sven <sven.hartwig@posteo.de>
2024-11-23 13:47:02 +0100

    Start new textrenderer implementation

---
+++ view/engine/TextRenderer/TextRendererNew.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <map>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include "../shaders/shader.h"
+
+struct Character
+{
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2 Size;        // Size of glyph
+    glm::ivec2 Bearing;     // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Offset to advance to next glyph
+};
+
+std::map<char, Character> Characters;
+
+class TextRenderer
+{
+public:
+    void RenderText(Shader &s, unsigned int VAO, unsigned int VBO, std::string text, float x, float y, float scale, glm::vec3 color);
+    int init();
+};
